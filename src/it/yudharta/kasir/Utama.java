4df1bc52b5779daedb07c11f1018dcec60ac1e4b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-03-04 12:41:49 +0700

    Class Utama

---
+++ src/it/yudharta/kasir/Utama.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 cahya
+ *
+ * Program ini adalah Aplikasi Gratis, Anda dapat Mengembangkan atau
+ * merubah sebagian atau seluruh kode, dengan seizin pengembang.
+ *
+ * Program ini dikembangkan dengan harapan agar bermanfaat,
+ * tapi tanpa GARANSI apapun.
+ */
+package it.yudharta.kasir;
+
+import it.yudharta.kasir.ui.MenuUtama;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ * Class Utama untuk menjalankan program Kasir.
+ *
+ * Seluruh program akan dijalankan mulai dari class Utama.
+ *
+ *
+ * @author cahya
+ */
+public class Utama {
+
+    /**
+     * Frame Menu Utama dibuat static agar bisa diakses dari class lain.
+     */
+    public static MenuUtama utama = MenuUtama.getInstance();
+
+    /**
+     * Menjalankan Frame Menu Utama.
+     *
+     * tampilan program akan di handle oleh Menu Utama.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            javax.swing.UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException | ClassNotFoundException ex) {
+            Logger.getLogger(Utama.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(()
+                -> utama.setVisible(true));
+    }
+
+}
