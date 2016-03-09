8ec4c03b59668057c679640953f66341b97a8ff6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-03-09 18:04:58 +0700

    center form
    agar form berada di tengah.

--- src/it/yudharta/kasir/ui/dialog/AturKoneksi.java
+++ src/it/yudharta/kasir/ui/dialog/AturKoneksi.java
@@ -42,6 +42,7 @@ public class AturKoneksi extends javax.swing.JDialog {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
